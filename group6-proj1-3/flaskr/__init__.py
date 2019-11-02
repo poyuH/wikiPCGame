be0a68a3012a871b674d15622ad0116f49975766
Po-Yu Huang <poyu0987@gmail.com>
2019-11-02 12:34:20 -0400

    finish front page

--- group6-proj1-3/flaskr/__init__.py
+++ group6-proj1-3/flaskr/__init__.py
@@ -9,7 +9,6 @@ def create_app(test_config=None):
     # create and configure the app
     tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
     app = Flask(__name__, instance_relative_config=True, template_folder=tmpl_dir)
-    my_db = db
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -24,40 +23,9 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # connects to our database
-    @app.before_request
-    def before_request():
-        my_db.start()
-
-    # close our database
-    @app.teardown_request
-    def teardown_request(exception):
-        my_db.close(exception)
-
-    """
     # register home page as blueprint
     app.register_blueprint(index.bp)
-    """
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        conn = my_db.get_conn()
-        cursor = conn.execute("SELECT cname FROM composer")
-        names = []
-        for result in cursor:
-          names.append(result['cname'])  # can also be accessed using result[0]
-        cursor.close()
-        context = dict(data = names)
-        return render_template("index.html", **context)
-
-    @app.route('/')
-    def home_page():
-        return index.home_page(my_db)
-
-    @app.route('/add', methods=['POST'])
-    def search():
-        return index.search(my_db, request)
-
+    app.add_url_rule('/', endpoint='index')
 
     return app
+
--- group6-proj1-3/flaskr/index.py
+++ group6-proj1-3/flaskr/index.py
@@ -1,22 +1,41 @@
-from flask import Flask, request, render_template, redirect, Response
+import functools
 from sqlalchemy import text
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from . import db
 
+bp = Blueprint('index', __name__)
+my_db = db
 
-def home_page(db, query=None):
-    conn = db.get_conn()
-    print(query)
+# connects to our database
+@bp.before_request
+def before_request():
+    my_db.start()
+
+# close our database
+@bp.teardown_request
+def teardown_request(exception):
+    my_db.close(exception)
+
+@bp.route('/<int:page_num>', methods=('GET', 'POST'))
+def home_page(page_num=0, query=None):
+    conn = my_db.get_conn()
     if not query:
-        cursor = conn.execute("SELECT gname FROM game ORDER BY date LIMIT 10 OFFSET 0")
+        cursor = conn.execute("SELECT gname FROM game ORDER BY date LIMIT 10 OFFSET %s;" % (10*page_num))
     else:
-        cursor = conn.execute(query)
+        cursor = conn.execute(text(query))
     names = []
     for result in cursor:
       names.append(result['gname'])
     cursor.close()
     context = dict(data = names)
+    context['next_page'] = page_num + 1
+    context['previous_page'] = page_num - 1
     return render_template("index.html", **context)
 
-def search(db, request):
+@bp.route('/search', methods=('GET', 'POST'))
+def search():
     query = "SELECT gname FROM game WHERE gname LIKE '%%%s%%' ORDER BY date" % request.form['name']
-    return home_page(db, query=text(query))
+    return home_page(query=query)
 
