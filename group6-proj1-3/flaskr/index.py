f67b1f262f74dd6e08d28b74423cc71a1523049d
Po-Yu Huang <poyu0987@gmail.com>
2019-11-10 18:16:27 -0500

    add wishlist to recommendation and append other uninteresting games to the recommendattion.

--- group6-proj1-3/flaskr/index.py
+++ group6-proj1-3/flaskr/index.py
@@ -31,8 +31,15 @@ def home_page(page_num=0, query=None):
                                   SELECT g.genre
                                   FROM game g, attend_transaction a, contain c
                                   WHERE g.gname=c.gname AND c.tid=a.tid AND a.account='%s')
+                                  OR
+                                  genre IN(
+                                  SELECT g.genre
+                                  FROM game g, wish_list w
+                                  WHERE g.gname=w.gname AND w.account='%s')
                                   ORDER BY date LIMIT 10 OFFSET %s;
-                                  """ % (g.user, (10*page_num)))
+                                  """ % (g.user, g.user, (10*page_num)))
+            if cursor.rowcount == 0:
+                cursor = conn.execute("SELECT gname FROM game ORDER BY date LIMIT 10 OFFSET %s;" % (10*page_num))
         else:
             cursor = conn.execute("SELECT gname FROM game ORDER BY date LIMIT 10 OFFSET %s;" % (10*page_num))
     else:
